c9e6b3e3be3c20a9152a4430f84bdfed969f69df
pankaj yadav <yadavpankaj.tech@gmail.com>
2025-07-10 11:44:48 +0000

    connect remote databse

--- backend/src/appMongoose.js
+++ backend/src/appMongoose.js
@@ -1,24 +1,17 @@
-const {connect} = require('mongoose');
+const { connect } = require('mongoose');
 
-
-
-const MONGO_URL = "mongodb://virat:virat123@localhost:27017";
-
-
-const DB_NAME = "library?authSource=admin";
+const MONGO_URL = "mongodb+srv://siri963690:siri963690@cluster111.ka2ei.mongodb.net";
+const DB_NAME = "library"; 
 
 const connectToDatabase = async () => {
   try {
-    await connect(`${MONGO_URL}/${DB_NAME}`);
+    await connect(`${MONGO_URL}/${DB_NAME}?retryWrites=true&w=majority`);
     console.log(`Connected to MongoDB at ${MONGO_URL}/${DB_NAME}`);
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
   }
-}
+};
 
 connectToDatabase();
 
-
 module.exports = {};
-
-
